8f3f3c427fa80d43e2498e67e7dca98ef61bb1a8
EbrahimGabriel <emgabriel@up.edu.ph>
2024-05-02 20:11:08 +0800

    added buttons to start screen

--- src/model/GameButton.java
+++ src/model/GameButton.java
@@ -1,5 +1,81 @@
 package model;
 
-public class GameButton {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
+
+public class GameButton extends Button {
+
+	private final String FONT_PATH = "src/model/resources/rainyhearts.ttf";
+	private final String BUTTON_PRESSED_STYLE = "-fx-background-image: url('/model/resources/red_button_pressed.png');";
+	private final String BUTTON_FREE_STYLE = "-fx-background-image: url('/model/resources/red_button_free.png');";
+
+	public GameButton(String text) {
+		setText(text);
+		setButtonFont();
+		setPrefWidth(190);
+		setPrefHeight(49);
+		setStyle(BUTTON_FREE_STYLE);
+		initializeButtonListeners();
+	}
+
+	private void setButtonFont() {
+		try {
+		setFont(Font.loadFont(new FileInputStream(FONT_PATH), 16));
+		} catch (FileNotFoundException e) {
+			setFont(Font.font("Verdana",16));
+		}
+	}
+
+	private void setButtonPressedStyle() {
+		setStyle(BUTTON_PRESSED_STYLE);
+		setPrefHeight(45);
+		setLayoutY(getLayoutY() + 4);
+	}
+
+	private void setButtonFreeStyle() {
+		setStyle(BUTTON_FREE_STYLE);
+		setPrefHeight(49);
+		setLayoutY(getLayoutY() - 4);
+	}
+
+	private void initializeButtonListeners() {
+		setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(event.getButton().equals(MouseButton.PRIMARY)) {
+					setButtonPressedStyle();
+				}
+			}
+		});
+
+		setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(event.getButton().equals(MouseButton.PRIMARY)) {
+					setButtonFreeStyle();
+				}
+			}
+		});
+
+		setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				setEffect(new DropShadow());
+			}
+		});
+
+		setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				setEffect(null);
+			}
+		});
+	}
 }
--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,42 +1,73 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
 import javafx.stage.Stage;
+import model.GameButton;
 
 public class ViewManager {
 
-	private static final int HEIGHT = 600;
-	private static final int WIDTH = 800;
+	private static final int HEIGHT = 500;
+	private static final int WIDTH = 700;
 
 	private AnchorPane mainPane;
 	private Scene mainScene;
 	private Stage mainStage;
 
+	private final static int MENU_BUTTON_START_X = 100;
+	private final static int MENU_BUTTON_START_Y = 150;
+
+	List<GameButton> menuButtons;
+
 	public ViewManager() {
+		menuButtons = new ArrayList<>();
 		mainPane = new AnchorPane();
 		mainScene = new Scene(mainPane,WIDTH,HEIGHT);
 		mainStage = new Stage();
 		mainStage.setScene(mainScene);
 		createButtons();
+		createBackground();
 	}
 
 	public Stage getMainStage() {
 		return mainStage;
 	}
 
-	private void createButtons() {
-		Button button = new Button();
+	private void addMenuButton(GameButton button) {
+		button.setLayoutX(MENU_BUTTON_START_X);
+		button.setLayoutY(MENU_BUTTON_START_Y + menuButtons.size() * 100);
+		menuButtons.add(button);
 		mainPane.getChildren().add(button);
+	}
+	private void createButtons() {
+		createStartButton();
+		createExitButton();
+	}
 
-		button.setOnMouseEntered(new EventHandler<Event>() {
-			@Override
-			public void handle(Event event) {
-				System.out.println("mouse entered");
-			}
-		});
+	private void createStartButton() {
+		GameButton startButton = new GameButton("Start");
+		addMenuButton(startButton);
+	}
+
+	private void createExitButton() {
+		GameButton startButton = new GameButton("Exit");
+		addMenuButton(startButton);
+	}
+
+	private void createBackground() {
+		Image backgroundImage = new Image("file:view/resources/background.jpg", 1000, 500, false, true);
+		BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+		mainPane.setBackground(new Background(background));
 	}
 }
