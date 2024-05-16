1acc078ba896de75f0a75d81a81a5798cbe58aad
JeaAnne18 <jdgadil@up.edu.ph>
2024-05-16 14:40:08 +0800

    added a pop up window and tried fixing the ui

--- src/model/GameSubScene.java
+++ src/model/GameSubScene.java
@@ -10,53 +10,57 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.util.Duration;
 
+import java.awt.Toolkit;
+
 //remove later
 import javafx.scene.text.Text;
 //---
-// this is the component that shows up when you click a button on the menu
+
 public class GameSubScene extends SubScene {
 
-	private final String FONT_PATH = "src/model/resources/rainyhearts.ttf";
-	private final String BACKGROUND_IMAGE = "model/resources/red_button_free.png";
+    private final String FONT_PATH = "src/model/resources/rainyhearts.ttf";
+    private final String BACKGROUND_IMAGE = "model/resources/red_button_free.png";
 
-	private boolean isHidden = true;
+    private boolean isHidden = true;
 
-	public GameSubScene() {
-		super(new AnchorPane(), 500, 300);
-		prefWidth(500);
-		prefHeight(300);
+    public GameSubScene() {
+        super(new AnchorPane(), 500, 300);
+        prefWidth(500);
+        prefHeight(300);
 
-		BackgroundImage image = new BackgroundImage(new Image(BACKGROUND_IMAGE, 500, 300, false, true),
-				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+        BackgroundImage image = new BackgroundImage(new Image(BACKGROUND_IMAGE, 500, 300, false, true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
 
-		AnchorPane root2 = (AnchorPane) this.getRoot();
-		root2.setBackground(new Background(image));
-		//remove later
-		Text t = new Text(10,50,"Test");
-		root2.getChildren().add(t);
-		//----
+        AnchorPane root2 = (AnchorPane) this.getRoot();
+        root2.setBackground(new Background(image));
 
-		setLayoutX(1024);
-		setLayoutY(180);
-	}
+        //remove later
+        Text t = new Text(10,50,"Test");
+        root2.getChildren().add(t);
+        //----
 
-	public void moveSubScene() {
-		TranslateTransition transition = new TranslateTransition();
-		transition.setDuration(Duration.seconds(0.3));
-		transition.setNode(this);
+        // Set initial position to be off-screen
+        setLayoutX(Toolkit.getDefaultToolkit().getScreenSize().width);
+        setLayoutY(180);
+    }
 
-		if (isHidden) {
-			transition.setToX(-676);
-			isHidden = false;
-		} else {
-			transition.setToX(0);
-			isHidden = true;
-		}
+    public void moveSubScene() {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(0.3));
+        transition.setNode(this);
 
-		transition.play();
-	}
+        if (isHidden) {
+            transition.setToX(-676);
+            isHidden = false;
+        } else {
+            transition.setToX(0);
+            isHidden = true;
+        }
 
-	public AnchorPane getPane() {
-		return (AnchorPane) this.getRoot();
-	}
+        transition.play();
+    }
+
+    public AnchorPane getPane() {
+        return (AnchorPane) this.getRoot();
+    }
 }
--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -25,218 +25,189 @@ import model.InfoLabel;
 //this is the view for the main menu and others
 public class ViewManager {
 
-	private static final int HEIGHT = 500;
-	private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 700;
 
-	private AnchorPane mainPane;
-	private Scene mainScene;
-	private Stage mainStage;
+    private AnchorPane mainPane;
+    private Scene mainScene;
+    private Stage mainStage;
 
-	private final static int MENU_BUTTON_START_X = 100;
-	private final static int MENU_BUTTON_START_Y = 150;
+    private final static int MENU_BUTTON_START_X = 100;
+    private final static int MENU_BUTTON_START_Y = 150;
 
-	private GameSubScene playSubScene;
-	private GameSubScene characterSelectSubScene;
-	private GameSubScene helpSubScene;
-	private GameSubScene creditsSubScene;
+    private GameSubScene playSubScene;
+    private GameSubScene characterSelectSubScene;
+    private GameSubScene helpSubScene;
+    private GameSubScene creditsSubScene;
 
-	private GameSubScene sceneToHide;
+    private GameSubScene sceneToHide;
 
-	List<GameButton> menuButtons;
+    List<GameButton> menuButtons;
 
-	List<CharacterSelect> characterList;
-	private CHARACTER chosenCharacter;
+    List<CharacterSelect> characterList;
+    private CHARACTER chosenCharacter;
 
-	public ViewManager() {
-		menuButtons = new ArrayList<>();
-		mainPane = new AnchorPane();
-		mainScene = new Scene(mainPane,WIDTH,HEIGHT);
-		mainStage = new Stage();
-		mainStage.setScene(mainScene);
+    public ViewManager() {
+        menuButtons = new ArrayList<>();
+        mainPane = new AnchorPane();
+        mainScene = new Scene(mainPane,WIDTH,HEIGHT);
+        mainStage = new Stage();
+        mainStage.setScene(mainScene);
 
-		createSubScenes();
-		createButtons();
-		createBackground();
-		//createTitle(); // uncomment when better title logo is found
+        createBackground();
+        createSubScenes();
+        createButtons();
+    }
 
-	}
+    public Stage getMainStage() {
+        return mainStage;
+    }
 
-	public Stage getMainStage() {
-		return mainStage;
-	}
+    private void showSubScene(GameSubScene subScene) {
+        if (sceneToHide != null) {
+            sceneToHide.moveSubScene();
+        }
 
-	//SubScene related Stuff
-	private void showSubScene(GameSubScene subScene) {
-		if (sceneToHide != null) {
-			sceneToHide.moveSubScene();
-		}
+        subScene.moveSubScene();
+        sceneToHide = subScene;
+    }
 
-		subScene.moveSubScene();
-		sceneToHide = subScene;
-	}
+    private void createSubScenes() {
 
-	private void createSubScenes() {
+        playSubScene = new GameSubScene();
+        mainPane.getChildren().add(playSubScene);
 
-		playSubScene = new GameSubScene();
-		mainPane.getChildren().add(playSubScene);
+        createCharacterSelectSubScene();
 
-		createCharacterSelectSubScene();
+        helpSubScene = new GameSubScene();
+        mainPane.getChildren().add(helpSubScene);
 
-		helpSubScene = new GameSubScene();
-		mainPane.getChildren().add(helpSubScene);
+        creditsSubScene = new GameSubScene();
+        mainPane.getChildren().add(creditsSubScene);
 
-		creditsSubScene = new GameSubScene();
-		mainPane.getChildren().add(creditsSubScene);
+    }
 
-	}
+    private void createCharacterSelectSubScene() {
+        characterSelectSubScene = new GameSubScene();
+        mainPane.getChildren().add(characterSelectSubScene);
 
-	private void createCharacterSelectSubScene() {
-		characterSelectSubScene = new GameSubScene();
-		mainPane.getChildren().add(characterSelectSubScene);
+        InfoLabel chooseCharacterLabel = new InfoLabel("Choose your character");
+        chooseCharacterLabel.setLayoutY(110);
+        chooseCharacterLabel.setLayoutY(25);
+        characterSelectSubScene.getPane().getChildren().add(chooseCharacterLabel);
+        characterSelectSubScene.getPane().getChildren().add(createCharactersToSelect());
+        characterSelectSubScene.getPane().getChildren().add(createPlayButton());
 
-		InfoLabel chooseCharacterLabel = new InfoLabel("Choose your character");
-		chooseCharacterLabel.setLayoutY(110);
-		chooseCharacterLabel.setLayoutY(25);
-		characterSelectSubScene.getPane().getChildren().add(chooseCharacterLabel);
-		characterSelectSubScene.getPane().getChildren().add(createCharactersToSelect());
-		characterSelectSubScene.getPane().getChildren().add(createPlayButton());
+    }
 
-	}
+    private HBox createCharactersToSelect() {
+        HBox box = new HBox();
+        box.setSpacing(20);
+        characterList = new ArrayList<>();
+        for (CHARACTER character: CHARACTER.values()) {
+            CharacterSelect characterToPick = new CharacterSelect(character);
+            characterList.add(characterToPick);
+            box.getChildren().add(characterToPick);
 
-	private HBox createCharactersToSelect() {
-		HBox box = new HBox();
-		box.setSpacing(20);
-		characterList = new ArrayList<>();
-		for (CHARACTER character: CHARACTER.values()) {
-			CharacterSelect characterToPick = new CharacterSelect(character);
-			characterList.add(characterToPick);
-			box.getChildren().add(characterToPick);
+            characterToPick.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    for (CharacterSelect character: characterList) {
+                        character.setIsCharacterChosen(false);
+                    }
+                    characterToPick.setIsCharacterChosen(true);
+                    chosenCharacter = characterToPick.getCharacter();
+                }
+            });
+        }
+        box.setLayoutX(300 - (110*2));
+        box.setLayoutY(100);
+        return box;
+    }
 
-			characterToPick.setOnMouseClicked(new EventHandler<MouseEvent>() {
-				@Override
-				public void handle(MouseEvent event) {
-					for (CharacterSelect character: characterList) {
-						character.setIsCharacterChosen(false);
-					}
-					characterToPick.setIsCharacterChosen(true);
-					chosenCharacter = characterToPick.getCharacter();
-				}
-			});
-		}
-		box.setLayoutX(300 - (110*2));
-		box.setLayoutY(100);
-		return box;
-	}
-	// ------
+    private void addMenuButton(GameButton button) {
+        button.setLayoutX(MENU_BUTTON_START_X);
+        button.setLayoutY(MENU_BUTTON_START_Y + menuButtons.size() * 100);
+        menuButtons.add(button);
+        mainPane.getChildren().add(button);
+    }
 
-	//Button related Stuff
-	private void addMenuButton(GameButton button) {
-		button.setLayoutX(MENU_BUTTON_START_X);
-		button.setLayoutY(MENU_BUTTON_START_Y + menuButtons.size() * 100);
-		menuButtons.add(button);
-		mainPane.getChildren().add(button);
-	}
+    private void createButtons() {
+        createStartButton();
+        createHelpButton();
+        createCreditsButton();
+        createExitButton();
+    }
 
-	private void createButtons() {
-		createStartButton();
-		createHelpButton();
-		createCreditsButton();
-		createExitButton();
-	}
+    private GameButton createPlayButton() {
+        GameButton playButton = new GameButton("Play");
+        playButton.setLayoutX(100);
+        playButton.setLayoutY(200);
 
-	private GameButton createPlayButton() {
-		GameButton playButton = new GameButton("Play");
-		playButton.setLayoutX(100);
-		playButton.setLayoutY(200);
+        playButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (chosenCharacter != null) {
+                    GameViewManager gameManager = new GameViewManager();
+                    gameManager.createNewGame(mainStage, chosenCharacter);
+                }
+            }
+        });
+        return playButton;
+    }
 
-		playButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				if (chosenCharacter != null) {
-					GameViewManager gameManager = new GameViewManager();
-					gameManager.createNewGame(mainStage, chosenCharacter);
-				}
-			}
-		});
-		return playButton;
-	}
+    private void createStartButton() {
+        GameButton startButton = new GameButton("Start");
+        addMenuButton(startButton);
 
-	private void createStartButton() {
-		GameButton startButton = new GameButton("Start");
-		addMenuButton(startButton);
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                showSubScene(characterSelectSubScene);
+            }
+        });
+    }
 
-		startButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				showSubScene(characterSelectSubScene);
-			}
-		});
-	}
+    private void createHelpButton() {
+        GameButton helpButton = new GameButton("Help");
+        addMenuButton(helpButton);
 
-	private void createHelpButton() {
-		GameButton helpButton = new GameButton("Help");
-		addMenuButton(helpButton);
+        helpButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                showSubScene(helpSubScene);
+            }
+        });
+    }
 
-		helpButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				showSubScene(helpSubScene);
-			}
-		});
-	}
+    private void createCreditsButton() {
+        GameButton creditsButton = new GameButton("Credits");
+        addMenuButton(creditsButton);
 
-	private void createCreditsButton() {
-		GameButton creditsButton = new GameButton("Credits");
-		addMenuButton(creditsButton);
+        creditsButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                showSubScene(creditsSubScene);
+            }
+        });
+    }
 
-		creditsButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				showSubScene(creditsSubScene);
-			}
-		});
-	}
+    private void createExitButton() {
+        GameButton exitButton = new GameButton("Exit");
+        addMenuButton(exitButton);
 
-	private void createExitButton() {
-		GameButton exitButton = new GameButton("Exit");
-		addMenuButton(exitButton);
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                mainStage.close();
+            }
+        });
+    }
 
-		exitButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				mainStage.close();
-			}
-		});
-	};
-
-	// ------
-	//Images on title screen
-	private void createBackground() {
-		Image backgroundImage = new Image("view/resources/background.jpg", 1000, 500, false, true);
-		BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
-		mainPane.setBackground(new Background(background));
-	}
-
-	private void createTitle() {
-		ImageView title = new ImageView("view/resources/background.jpg"); //missing image
-		title.setLayoutX(400);
-		title.setLayoutY(50);
-
-		title.setOnMouseEntered(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				title.setEffect(new DropShadow());
-			}
-		});
-
-		title.setOnMouseExited(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				title.setEffect(null);
-			}
-		});
-
-		mainPane.getChildren().add(title);
-	}
-	// ------
+    private void createBackground() {
+        ImageView backgroundImage = new ImageView(new Image("view/resources/background_1.gif"));
+        backgroundImage.fitWidthProperty().bind(mainPane.widthProperty());
+        backgroundImage.fitHeightProperty().bind(mainPane.heightProperty());
+        mainPane.getChildren().add(backgroundImage);
+    }
 }
