abba2ede69d56525e5d605556a7d366dc75dfe06
EbrahimGabriel <emgabriel@up.edu.ph>
2024-05-31 07:59:37 +0800

    returned collisions

--- src/view/GameViewManager.java
+++ src/view/GameViewManager.java
@@ -178,7 +178,7 @@ public class GameViewManager {
             public void handle(long now) {
                 moveCharacter();
                 checkIfElementsCollide();
-//                checkPlayerCollisions();
+                checkPlayerCollisions();
             }
         };
 
